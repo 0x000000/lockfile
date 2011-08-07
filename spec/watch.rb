64c40ce4747d72e7431007ebe96395a06671b44a
Alexander Rudenko <mur.mailbox@gmail.com>
2011-08-07 20:29:22 +0300

    Bundler check was added. Removed unused code. Changed rspec shell command

--- spec/watch.rb
+++ spec/watch.rb
@@ -1,19 +1,21 @@
+require "rubygems"
+require "bundler/setup"
+
 # A simple alternative to autotest that isnt as painful
 puts ">> Watching spec folder for changes..."
 
 options = {
-  :options => "--require 'spec/spec_helper' --format nested --color",
-  :binary => "spec"
+  :options => "--require '#{File.dirname(__FILE__)}/spec_helper' --format nested --color",
+  :binary => "rspec"
 }
 
-watch("lib/(.*)\.rb") do |match|
+watch("lib/(.*)\.rb") do
   puts %x[ clear ]
   
-  file   = match[match.size - 1]
   opts   = options[:options]
   binary = options[:binary]
   files  = []
-  
+
   %w{ lib }.each do |dir|
     ["spec/#{dir}/*/*.rb", "spec/#{dir}/*.rb"].each do |glob|
       Dir.glob(glob).each { |f| files << f }
@@ -24,7 +26,7 @@ watch("lib/(.*)\.rb") do |match|
   files.each { |f| puts "+ #{f}" }
   puts ""
   command = "#{binary} #{files.collect! { |f| File.expand_path(f) }.join(" ")} #{opts}"
-  
+
   system(command)
   
 end
